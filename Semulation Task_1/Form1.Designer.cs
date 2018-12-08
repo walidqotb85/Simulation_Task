e204c9f577e84a4e7df95edfa5844e1637eaffef
Walid Qotb Yossef <walidkotb9@gmail.com>
2018-12-08 21:29:53 +0200

    My last commit

--- Semulation Task_1/Form1.Designer.cs
+++ Semulation Task_1/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DGV_customers_times = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.customersChar = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.loadDiagram_Btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_customers_times)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.customersChar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DGV_customers_times);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(543, 448);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Customers Informations";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // DGV_customers_times
+            // 
+            this.DGV_customers_times.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_customers_times.Location = new System.Drawing.Point(6, 22);
+            this.DGV_customers_times.Name = "DGV_customers_times";
+            this.DGV_customers_times.Size = new System.Drawing.Size(531, 418);
+            this.DGV_customers_times.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.loadDiagram_Btn);
+            this.groupBox2.Controls.Add(this.customersChar);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(561, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(418, 440);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Diagram For Customers";
+            // 
+            // customersChar
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.customersChar.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.customersChar.Legends.Add(legend1);
+            this.customersChar.Location = new System.Drawing.Point(6, 22);
+            this.customersChar.Name = "customersChar";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Arrival Time";
+            series2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "End Time";
+            this.customersChar.Series.Add(series1);
+            this.customersChar.Series.Add(series2);
+            this.customersChar.Size = new System.Drawing.Size(405, 373);
+            this.customersChar.TabIndex = 0;
+            this.customersChar.Text = "chart1";
+            // 
+            // loadDiagram_Btn
+            // 
+            this.loadDiagram_Btn.Location = new System.Drawing.Point(128, 401);
+            this.loadDiagram_Btn.Name = "loadDiagram_Btn";
+            this.loadDiagram_Btn.Size = new System.Drawing.Size(126, 33);
+            this.loadDiagram_Btn.TabIndex = 1;
+            this.loadDiagram_Btn.Text = "Load Diagram";
+            this.loadDiagram_Btn.UseVisualStyleBackColor = true;
+            this.loadDiagram_Btn.Click += new System.EventHandler(this.loadDiagram_Btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 464);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_customers_times)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.customersChar)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView DGV_customers_times;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button loadDiagram_Btn;
+        private System.Windows.Forms.DataVisualization.Charting.Chart customersChar;
     }
 }
 
--- Semulation Task_1/Form1.cs
+++ Semulation Task_1/Form1.cs
@@ -10,11 +10,134 @@ using System.Windows.Forms;
 
 namespace Semulation_Task_1
 {
+
     public partial class Form1 : Form
     {
+        int[] arrivalTime = new int[8];
+        int[] endTime = new int[8];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable customers_times = new DataTable();
+            Random random_time = new Random();
+
+            customers_times.Columns.Add("Customer Number", typeof(int));
+            customers_times.Columns.Add("Arrival Time", typeof(int));
+            customers_times.Columns.Add("Time Service Begins", typeof(int));
+            customers_times.Columns.Add("Service Time Duration", typeof(int));
+            customers_times.Columns.Add("Time Service Ends", typeof(int));
+
+            //customer1
+            int SeviceTimeDuration =  random_time.Next(1, 3);
+            customers_times.Rows.Add(1, 0, 0, SeviceTimeDuration, SeviceTimeDuration);
+            arrivalTime[0] = 0;
+            endTime[0] = SeviceTimeDuration;
+
+            //Customer2
+            int arrivalTime2 = random_time.Next(3, 7);
+            int timeServiceBegains2 = random_time.Next(3,7);
+            int serviceTimeDuration2 = random_time.Next(1, 5);
+            int timeServiceEnd2 = timeServiceBegains2 + serviceTimeDuration2;
+            customers_times.Rows.Add(2, arrivalTime2, timeServiceBegains2, serviceTimeDuration2, timeServiceEnd2);
+            arrivalTime[1] = arrivalTime2;
+            endTime[1] = timeServiceEnd2;
+
+
+            //customer3
+            int arrivalTime3 = random_time.Next(8, 12);
+            int timeServiceBegains3 = random_time.Next(4, 8);
+            int serviceTimeDuration3 = random_time.Next(1, 5);
+            int timeServiceEnd3 = timeServiceBegains3 + serviceTimeDuration3;
+            customers_times.Rows.Add(3, arrivalTime3, timeServiceBegains3, serviceTimeDuration3, timeServiceEnd3);
+            arrivalTime[2] = arrivalTime3;
+            endTime[2] = timeServiceEnd3;
+
+            //customer4
+            int arrivalTime4 = random_time.Next(12, 16);
+            int timeServiceBegains4 = random_time.Next(8,12);
+            int serviceTimeDuration4 = random_time.Next(1, 5);
+            int timeServiceEnd4 = timeServiceBegains4 + serviceTimeDuration4;
+            customers_times.Rows.Add(4, arrivalTime4, timeServiceBegains4, serviceTimeDuration4, timeServiceEnd4);
+            arrivalTime[3] = arrivalTime4;
+            endTime[3] = timeServiceEnd4;
+
+            //customer5
+            int arrivalTime5 = random_time.Next(16, 20);
+            int timeServiceBegains5 = random_time.Next(12, 16);
+            int serviceTimeDuration5 = random_time.Next(1, 5);
+            int timeServiceEnd5 = timeServiceBegains5 + serviceTimeDuration5;
+            customers_times.Rows.Add(5, arrivalTime5, timeServiceBegains5, serviceTimeDuration5, timeServiceEnd5);
+            arrivalTime[4] = arrivalTime5;
+            endTime[4] = timeServiceEnd5;
+
+            //customer6
+            int arrivalTime6 = random_time.Next(20, 24);
+            int timeServiceBegains6 = random_time.Next(16, 20);
+            int serviceTimeDuration6 = random_time.Next(1, 5);
+            int timeServiceEnd6 = timeServiceBegains6 + serviceTimeDuration6;
+            customers_times.Rows.Add(6, arrivalTime6, timeServiceBegains6, serviceTimeDuration6, timeServiceEnd6);
+            arrivalTime[5] = arrivalTime6;
+            endTime[5] = timeServiceEnd6;
+
+            //customer7
+            int arrivalTime7 = random_time.Next(24, 28);
+            int timeServiceBegains7 = random_time.Next(20, 24);
+            int serviceTimeDuration7 = random_time.Next(1, 5);
+            int timeServiceEnd7 = timeServiceBegains7 + serviceTimeDuration7;
+            customers_times.Rows.Add(7, arrivalTime7, timeServiceBegains7, serviceTimeDuration7, timeServiceEnd7);
+            arrivalTime[6] = arrivalTime7;
+            endTime[6] = timeServiceEnd7;
+
+            //customer8
+            int arrivalTime8 = random_time.Next(28, 32);
+            int timeServiceBegains8 = random_time.Next(24, 28);
+            int serviceTimeDuration8 = random_time.Next(1, 5);
+            int timeServiceEnd8 = timeServiceBegains8 + serviceTimeDuration8;
+            customers_times.Rows.Add(8, arrivalTime8, timeServiceBegains8, serviceTimeDuration8, timeServiceEnd8);
+            arrivalTime[7] = arrivalTime8;
+            endTime[7] = timeServiceEnd8;
+
+            DGV_customers_times.DataSource = customers_times;
+        }
+
+        private void loadDiagram_Btn_Click(object sender, EventArgs e)
+        {
+            this.customersChar.Series["Arrival Time"].Points.AddXY(1, arrivalTime[0]);
+            this.customersChar.Series["End Time"].Points.AddXY(1, endTime[0]);
+
+            this.customersChar.Series["Arrival Time"].Points.AddXY(2, arrivalTime[1]);
+            this.customersChar.Series["End Time"].Points.AddXY(2, endTime[1]);
+
+            this.customersChar.Series["Arrival Time"].Points.AddXY(3, arrivalTime[2]);
+            this.customersChar.Series["End Time"].Points.AddXY(3, endTime[2]);
+
+            this.customersChar.Series["Arrival Time"].Points.AddXY(4, arrivalTime[3]);
+            this.customersChar.Series["End Time"].Points.AddXY(4, endTime[3]);
+
+            this.customersChar.Series["Arrival Time"].Points.AddXY(5, arrivalTime[4]);
+            this.customersChar.Series["End Time"].Points.AddXY(5, endTime[4]);
+
+            this.customersChar.Series["Arrival Time"].Points.AddXY(6, arrivalTime[5]);
+            this.customersChar.Series["End Time"].Points.AddXY(6, endTime[5]);
+
+            this.customersChar.Series["Arrival Time"].Points.AddXY(7, arrivalTime[6]);
+            this.customersChar.Series["End Time"].Points.AddXY(7, endTime[6]);
+
+            this.customersChar.Series["Arrival Time"].Points.AddXY(8, arrivalTime[7]);
+            this.customersChar.Series["End Time"].Points.AddXY(8, endTime[7]);
+
+
+
         }
     }
 }
